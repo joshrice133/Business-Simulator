6e08d424bd11f3cf2ee587aa8dde141622e5827f
jteerice <jake.rice@trojans.dsu.edu>
2025-07-17 14:30:00 -0400

    Windows starts correctly

--- MainGame.py
+++ MainGame.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from config import *
 
@@ -6,13 +7,24 @@ class MainGame:
         pygame.init()
         self.screen = pygame.display.set_mode((RESOLUTION_X, RESOLUTION_Y))
         pygame.display.set_caption("Business Simulator")
+
     def quit(self):
+        pygame.quit()
+        sys.exit()
+
     def checkEvents(self):
+        return pygame.event.get()
+
+    def render(self):
+        self.screen.fill((0, 0, 0))
+        pygame.display.flip()
+
     def run(self):
         while True:
             for event in self.checkEvents():
                 if event.type == pygame.QUIT:
                     self.quit()
                 self.render()
-    def render(self):
 
+if __name__ == "__main__":
+    MainGame().run()
